1c050c731180a06583516a82233561d2dae6c418
Atsuko Nemoto <98293619+nemotoaa@users.noreply.github.com>
2022-02-07 15:30:16 +0900

    modify operationfilter test
    Signed-off-by: Atsuko Nemoto <98293619+nemotoaa@users.noreply.github.com>

--- tests/unit/gui/lib/operationHistory/scriptGenerator/model/pageObject/method/operation/DuplicateElementOperationFilter.spec.ts
+++ tests/unit/gui/lib/operationHistory/scriptGenerator/model/pageObject/method/operation/DuplicateElementOperationFilter.spec.ts
@@ -24,7 +24,7 @@ describe("DuplicateElementOperationFilter", () => {
       expect(operations).toEqual([operation1]);
     });
 
-    it("操作対象要素の識別子が空の操作は除外する", () => {
+    it("操作対象要素の識別子が空の操作は除外しない", () => {
       const operation1: PageObjectOperation = {
         target: {
           identifier: "",
@@ -39,7 +39,7 @@ describe("DuplicateElementOperationFilter", () => {
         operation1,
       ]);
 
-      expect(operations).toEqual([]);
+      expect(operations).toEqual([operation1]);
     });
 
     it("画面遷移するまでに同一の要素への操作が複数ある場合は最後のもの以外を除外する", () => {
