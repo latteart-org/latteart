723467d1fc25af94c8a8f281f3bb11db715d7d79
Hiroyuki Kirinuki <hiro5051@gmail.com>
2022-02-25 11:25:13 +0900

    Fix some messages for JsDoc (#96)
    Signed-off-by: Hiroyuki Kirinuki <hiro5051@gmail.com>

--- src/gui/lib/operationHistory/scriptGenerator/codeGenerator/js/other/JSDocReadmeGenerator.ts
+++ src/gui/lib/operationHistory/scriptGenerator/codeGenerator/js/other/JSDocReadmeGenerator.ts
@@ -70,7 +70,7 @@ ${this.buildPageObjectTable(pageObjectInfos)}
 
         return `|${index + 1}|<a href="${href}">${alias}</a>|${name}|${
           invalidTypeExists
-            ? `<span style="color:red">Some operation was not generated. Please read details using the left link.</span>`
+            ? `<span style="color:red">Code for some operations is not generated. Please click the link on the left for more information.</span>`
             : ""
         }|`;
       })
