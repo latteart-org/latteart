95c57989fae74f1261f399990b4783b454be3026
Mamoru Yanagisawa <72236660+mamoru-x-yanagisawa@users.noreply.github.com>
2022-09-20 12:53:36 +0900

    async config
    Signed-off-by: Mamoru Yanagisawa <72236660+mamoru-x-yanagisawa@users.noreply.github.com>

--- src/gui/vue/pages/operationHistory/organisms/configViewer/CoverageSetting.vue
+++ src/gui/vue/pages/operationHistory/organisms/configViewer/CoverageSetting.vue
@@ -19,7 +19,7 @@
     <div class="head-label">
       {{ $store.getters.message("config-view.include-coverage") }}
     </div>
-    <v-flex v-if="tagList.length <= 0">
+    <v-flex v-if="tempTagList.length <= 0">
       <v-card color="#EEE">
         <v-card-text>
           <v-layout align-center>
@@ -28,7 +28,12 @@
         </v-card-text>
       </v-card>
     </v-flex>
-    <v-flex style="width: 150px" v-for="tag in tagList" v-bind:key="tag" pa-1>
+    <v-flex
+      style="width: 150px"
+      v-for="tag in tempTagList"
+      v-bind:key="tag"
+      pa-1
+    >
       <v-card class="ma-1 pa-1" color="#EEE">
         <v-card-text class="my-0 py-0">
           <v-layout align-center ma-0 pa-0>
@@ -36,7 +41,7 @@
               {{ tag }}
             </v-flex>
             <v-flex xs2 ma-0 pa-0 align-right>
-              <v-checkbox v-model="tags" :value="tag" />
+              <v-checkbox v-model="tempTags" :value="tag" />
             </v-flex>
           </v-layout>
         </v-card-text>
@@ -55,24 +60,24 @@ export default class CoverageSetting extends Vue {
   public readonly includeTags!: string[];
   @Prop({ type: Array, default: () => [] }) public defaultTagList!: string[];
 
-  private inclusionTagMap = new Map();
-  private tags: string[] = [];
-  private displayInclusionList: string[] = [];
-  private tagList: string[] = [];
+  private tempInclusionTagMap = new Map();
+  private tempTags: string[] = [];
+  private tempDisplayInclusionList: string[] = [];
+  private tempTagList: string[] = [];
 
   @Watch("includeTags")
-  private changeIncludeTags() {
-    this.tags = this.includeTags;
-    this.displayInclusionList = Array.from(
-      new Set(this.tags.concat(this.defaultTagList))
+  private updateTempConfig() {
+    this.tempTags = this.includeTags;
+    this.tempDisplayInclusionList = Array.from(
+      new Set(this.tempTags.concat(this.defaultTagList))
     );
     this.createInclusionTagMap();
   }
 
-  @Watch("tags")
+  @Watch("tempTags")
   private saveTags() {
     this.$emit("save-config", {
-      coverage: { include: { tags: this.tags } },
+      coverage: { include: { tags: this.tempTags } },
     });
     this.createInclusionTagMap();
   }
@@ -83,32 +88,34 @@ export default class CoverageSetting extends Vue {
       return;
     }
 
-    this.tagList = this.displayInclusionList.sort((a: string, b: string) => {
-      const aVal = this.inclusionTagMap.get(a);
-      const bVal = this.inclusionTagMap.get(b);
-      if (!!aVal > !!bVal) {
-        return -1;
-      } else if (!!aVal < !!bVal) {
-        return 1;
+    this.tempTagList = this.tempDisplayInclusionList.sort(
+      (a: string, b: string) => {
+        const aVal = this.tempInclusionTagMap.get(a);
+        const bVal = this.tempInclusionTagMap.get(b);
+        if (!!aVal > !!bVal) {
+          return -1;
+        } else if (!!aVal < !!bVal) {
+          return 1;
+        }
+        if (a < b) {
+          return -1;
+        } else if (a > b) {
+          return 1;
+        }
+        return 0;
       }
-      if (a < b) {
-        return -1;
-      } else if (a > b) {
-        return 1;
-      }
-      return 0;
-    });
+    );
   }
 
   private createInclusionTagMap() {
     const tagMap = new Map();
-    this.displayInclusionList.forEach((tag: string) => {
+    this.tempDisplayInclusionList.forEach((tag: string) => {
       tagMap.set(tag, false);
     });
-    this.tags.forEach((tag) => {
+    this.tempTags.forEach((tag) => {
       tagMap.set(tag, true);
     });
-    this.inclusionTagMap = tagMap;
+    this.tempInclusionTagMap = tagMap;
   }
 }
 </script>
