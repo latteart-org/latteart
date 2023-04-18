e43e2ed12d3520d7366735323743688c35ef4d37
Atsuko Nemoto <98293619+nemotoaa@users.noreply.github.com>
2023-04-18 14:18:05 +0900

    fix selectable table
    Signed-off-by: Atsuko Nemoto <98293619+nemotoaa@users.noreply.github.com>

--- packages/latteart-gui/src/components/molecules/SelectableDataTable.vue
+++ packages/latteart-gui/src/components/molecules/SelectableDataTable.vue
@@ -24,10 +24,9 @@
       :hide-default-header="hideHeaders"
       :hide-default-footer="hideFooters"
       must-sort
-      show-select
     >
-      <template v-slot:header="props">
-        <tr :style="{ height: '40px !important' }">
+      <template v-slot:header="{ props }">
+        <tr :style="{ height: '40px !important' }" class="v-data-table-header">
           <th class="check-col" v-if="!hideCheckBox">
             <v-checkbox
               :input-value="props.all"
@@ -52,7 +51,9 @@
             @click="changeSort(header.value)"
           >
             {{ header.text }}
-            <v-icon small>arrow_upward</v-icon>
+            <v-icon class="v-data-table-header__icon" small
+              >arrow_upward</v-icon
+            >
           </th>
         </tr>
       </template>
@@ -175,12 +176,13 @@ export default class SelectableDataTable<T> extends Vue {
   }
 
   private get tableHeaders() {
-    return this.headers.map((header) => {
+    const a = this.headers.map((header) => {
       return {
         ...header,
         value: header.value,
       };
     });
+    return a;
   }
 
   private get visibleItems() {
