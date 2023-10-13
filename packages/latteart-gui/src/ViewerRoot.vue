e236ccdbdc469486966f3e0bf01f386d543e4490
Atsuko Nemoto <98293619+nemotoaa@users.noreply.github.com>
2023-10-13 17:07:57 +0900

    fix #1081 add side menu to snapshot (#1094)
    Signed-off-by: Atsuko Nemoto <98293619+nemotoaa@users.noreply.github.com>

--- packages/latteart-gui/src/ViewerRoot.vue
+++ packages/latteart-gui/src/ViewerRoot.vue
@@ -16,17 +16,114 @@
 
 <template>
   <v-app>
-    <router-view></router-view>
+    <v-navigation-drawer app :mini-variant.sync="mini" permanent>
+      <v-list-item class="px-2">
+        <v-list-item-avatar>
+          <v-img src="./assets/logo.png"></v-img>
+        </v-list-item-avatar>
+        <v-list-item-content>
+          <v-list-item-title class="text-h6"> LatteArt </v-list-item-title>
+        </v-list-item-content>
+
+        <v-btn icon @click.stop="mini = !mini">
+          <v-icon>chevron_left</v-icon>
+        </v-btn>
+      </v-list-item>
+
+      <v-divider></v-divider>
+
+      <v-list dense nav>
+        <v-subheader v-if="!mini">{{
+          $store.getters.message("navigation.group-label.management")
+        }}</v-subheader>
+
+        <v-list-item-group v-model="displayedPage" color="primary">
+          <v-list-item
+            :disabled="!hasTestMatrix"
+            to="/manage/view/show"
+            :title="$store.getters.message('manage-header.top')"
+            exact
+          >
+            <v-list-item-icon>
+              <v-icon :disabled="!hasTestMatrix">calendar_today</v-icon>
+            </v-list-item-icon>
+
+            <v-list-item-content>
+              <v-list-item-title>{{
+                $store.getters.message("manage-header.top")
+              }}</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+
+          <v-list-item
+            :disabled="!hasSession"
+            to="/manage/view/progress"
+            :title="$store.getters.message('manage-progress.title')"
+            exact
+          >
+            <v-list-item-icon>
+              <v-icon :disabled="!hasSession">waterfall_chart</v-icon>
+            </v-list-item-icon>
+
+            <v-list-item-content>
+              <v-list-item-title>{{
+                $store.getters.message("manage-progress.title")
+              }}</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+
+          <v-list-item
+            :disabled="!hasSession"
+            to="/manage/view/quality"
+            :title="$store.getters.message('manage-quality.title')"
+            exact
+          >
+            <v-list-item-icon>
+              <v-icon :disabled="!hasSession">show_chart</v-icon>
+            </v-list-item-icon>
+
+            <v-list-item-content>
+              <v-list-item-title>{{
+                $store.getters.message("manage-quality.title")
+              }}</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-main>
+      <router-view></router-view>
+    </v-main>
   </v-app>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
+import { TestMatrix } from "./lib/testManagement/types";
+import { TestManagementState } from "./store/testManagement";
 
 @Component
 export default class Root extends Vue {
+  private mini = false;
+  private displayedPage = 0;
+
   private mounted(): void {
     this.$router.push({ name: "manageShowView" });
+  }
+
+  private get hasTestMatrix(): boolean {
+    const testMatrices: TestMatrix[] =
+      this.$store.getters["testManagement/getTestMatrices"]();
+
+    return testMatrices.length > 0;
+  }
+
+  private get hasSession(): boolean {
+    const stories = (this.$store.state.testManagement as TestManagementState)
+      .stories;
+
+    return stories.flatMap((story) => story.sessions).length > 0;
   }
 }
 </script>
