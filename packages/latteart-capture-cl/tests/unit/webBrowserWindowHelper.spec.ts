77b2513931de9e3896e5b40c3c489a70351a6bab
Mamoru Yanagisawa <72236660+mamoru-x-yanagisawa@users.noreply.github.com>
2023-11-16 15:44:34 +0900

    fix #1189 fix unit test
    Signed-off-by: Mamoru Yanagisawa <72236660+mamoru-x-yanagisawa@users.noreply.github.com>

--- packages/latteart-capture-cl/tests/unit/webBrowserWindowHelper.spec.ts
+++ packages/latteart-capture-cl/tests/unit/webBrowserWindowHelper.spec.ts
@@ -4,7 +4,7 @@ import {
 } from "@/capturer/browser/window/webBrowserWindowHelper";
 
 describe("isIgnoreOperation", () => {
-  describe("指定の操作が記録対象外の場合はtrueを返す", () => {
+  describe("指定の操作が記録対象外でない場合はfalseを返す", () => {
     describe("単独で発火したイベント", () => {
       it.each`
         eventType   | targetTagname | targetAttributes
@@ -84,7 +84,7 @@ describe("isIgnoreOperation", () => {
     });
   });
 
-  describe("指定の操作が記録対象外でない場合はfalseを返す", () => {
+  describe("指定の操作が記録対象外の場合はtrueを返す", () => {
     describe("単独で発火したイベント", () => {
       it.each`
         eventType  | targetTagname | targetAttributes
@@ -115,6 +115,11 @@ describe("isIgnoreOperation", () => {
         ${"click"}    | ${"INPUT"}        | ${{ type: "radio" }}     | ${"xpath1"}     | ${"change"} | ${"INPUT"}    | ${{ type: "radio" }}                      | ${"xpath1"}
         ${"click"}    | ${"INPUT"}        | ${{ type: "checkbox" }}  | ${"xpath1"}     | ${"change"} | ${"INPUT"}    | ${{ type: "checkbox" }}                   | ${"xpath1"}
         ${"click"}    | ${"LABEL"}        | ${{ for: "checkboxId" }} | ${"xpath1"}     | ${"click"}  | ${"INPUT"}    | ${{ id: "checkboxId", type: "checkbox" }} | ${"xpath2"}
+        ${"click"}    | ${"LABEL"}        | ${{ for: "radioId" }}    | ${"xpath1"}     | ${"click"}  | ${"INPUT"}    | ${{ id: "radioId", type: "radio" }}       | ${"xpath2"}
+        ${"click"}    | ${"LABEL"}        | ${{ for: "textId" }}     | ${"xpath1"}     | ${"click"}  | ${"INPUT"}    | ${{ id: "textId", type: "text" }}         | ${"xpath2"}
+        ${"click"}    | ${"LABEL"}        | ${{ for: "passwordId" }} | ${"xpath1"}     | ${"click"}  | ${"INPUT"}    | ${{ id: "passwordId", type: "password" }} | ${"xpath2"}
+        ${"click"}    | ${"LABEL"}        | ${{ for: "emailId" }}    | ${"xpath1"}     | ${"click"}  | ${"INPUT"}    | ${{ id: "emailId", type: "email" }}       | ${"xpath2"}
+        ${"click"}    | ${"LABEL"}        | ${{ for: "telId" }}      | ${"xpath1"}     | ${"click"}  | ${"INPUT"}    | ${{ id: "telId", type: "tel" }}           | ${"xpath2"}
       `(
         "$prevEventType: $prevTargetTagname $prevTargetAttributes $prevTargetXpath => $eventType: $targetTagname $targetAttributes $targetXpath",
         async ({
