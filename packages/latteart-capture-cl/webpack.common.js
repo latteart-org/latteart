9d64692003bde683d64bf92275173e502346b679
Ryohei Fujiwara <70507073+fujiwr@users.noreply.github.com>
2025-02-07 12:55:11 +0900

    fix #1655 node package (#1672)
    Signed-off-by: Ryohei Fujiwara <70507073+fujiwr@users.noreply.github.com>

--- packages/latteart-capture-cl/webpack.common.js
+++ packages/latteart-capture-cl/webpack.common.js
@@ -19,15 +19,6 @@ module.exports = {
       ],
     }),
   ],
-  module: {
-    rules: [
-      {
-        test: /\.ts$/,
-        use: "ts-loader",
-        exclude: /node_modules/,
-      },
-    ],
-  },
   resolve: {
     extensions: [".ts", ".js"],
     alias: {
--- packages/latteart-repository/webpack.prod.js
+++ packages/latteart-repository/webpack.prod.js
@@ -2,7 +2,27 @@
 const { merge } = require("webpack-merge");
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const common = require("./webpack.common.js");
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const path = require("path");
 
 module.exports = merge(common, {
   mode: "production",
+  optimization: { minimize: false },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: {
+          loader: "ts-loader",
+          options: {
+            configFile: path.resolve(__dirname, "tsconfig.prod.json"),
+          },
+        },
+        exclude: /node_modules/,
+      },
+    ],
+  },
 });
