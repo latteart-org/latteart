8dff999194080ce9c0378fd5cd66d3ea60d77fe4
Mamoru Yanagisawa <72236660+mamoru-x-yanagisawa@users.noreply.github.com>
2025-02-21 14:26:22 +0900

    fix #1696 update copyright to 2025
    Signed-off-by: Mamoru Yanagisawa <72236660+mamoru-x-yanagisawa@users.noreply.github.com>

--- packages/latteart-gui/src/components/organisms/common/TestHintList.vue
+++ packages/latteart-gui/src/components/organisms/common/TestHintList.vue
@@ -1,5 +1,5 @@
 <!--
- Copyright 2024 NTT Corporation.
+ Copyright 2025 NTT Corporation.
 
  Licensed under the Apache License, Version 2.0 (the "License");
  you may not use this file except in compliance with the License.
@@ -285,7 +285,7 @@ export default defineComponent({
                 const customProp = props.testHintProps.find(({ id }) => id === custom.propId);
                 const value = customProp
                   ? customProp.type === "list"
-                    ? customProp.listItems?.find(({ key }) => key === custom.value)?.value ?? ""
+                    ? (customProp.listItems?.find(({ key }) => key === custom.value)?.value ?? "")
                     : custom.value
                   : "";
                 return [custom.propId, value];
