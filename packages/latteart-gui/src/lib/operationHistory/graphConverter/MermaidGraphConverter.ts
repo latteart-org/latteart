c03c7e18bee1dfb9075dd6a776c91d5966d9f3ac
Ryohei Fujiwara <70507073+fujiwr@users.noreply.github.com>
2025-02-27 13:38:09 +0900

    fix #1709 downgrade mermaid version
    Signed-off-by: Ryohei Fujiwara <70507073+fujiwr@users.noreply.github.com>

--- packages/latteart-gui/src/lib/operationHistory/graphConverter/MermaidGraphConverter.ts
+++ packages/latteart-gui/src/lib/operationHistory/graphConverter/MermaidGraphConverter.ts
@@ -25,7 +25,7 @@ export default class MermaidGraphConverter {
    * @param theme
    */
   constructor(theme: "default" | "forest" | "dark" | "neutral" = "default") {
-    Mermaid.initialize({
+    Mermaid.mermaidAPI.initialize({
       startOnLoad: true,
       theme,
       fontSize: undefined
@@ -38,14 +38,21 @@ export default class MermaidGraphConverter {
    * @param graphText  Graph text to convert.
    * @returns svg
    */
-  public async toSVG(svgId: string, graphText: string): Promise<string> {
+  public toSVG(svgId: string, graphText: string): string {
     // Create a container applied a 'font-family' before rendering.
-    const container = document.createElement("div") as Element;
-    (container as any).style.fontFamily = '"trebuchet ms", verdana, arial';
+    const container = document.createElement("div");
+    container.style.fontFamily = '"trebuchet ms", verdana, arial';
     document.body.insertAdjacentElement("beforeend", container);
 
-    const svg = await Mermaid.render(svgId, graphText, container);
+    const svg = Mermaid.render(
+      svgId,
+      graphText,
+      () => {
+        /* Do nothing */
+      },
+      container as any
+    );
     container.remove();
-    return svg.svg;
+    return svg;
   }
 }
